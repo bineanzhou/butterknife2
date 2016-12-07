df8c4893b4a87006d64fd5b3ea715d5048ec13ae
zhoubin <zhoubin@nicaifu.com>
2016-12-07 19:43:19 +0800

    V7.0.1

--- app/src/main/java/org/butterknife/MainActivity.java
+++ app/src/main/java/org/butterknife/MainActivity.java
@@ -2,15 +2,23 @@ package org.butterknife;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import org.butterknife.processor.TestAnnotation;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-@TestAnnotation
+
 public class MainActivity extends Activity {
+
+    @Bind(R.id.tv)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        if(mTextView!=null)
+            mTextView.setText("test Bind");
     }
 }
